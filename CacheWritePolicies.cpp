262897dc437cc447a6aa14b891606d7e8880b826
Scimmiaparlante <Matteo@DESKTOP-H7M02I7>
2019-05-15 15:58:02 +0200

    mod8

--- CacheWritePolicies.cpp
+++ CacheWritePolicies.cpp
@@ -31,25 +31,25 @@ void CacheWritePolicies::onNotify(message *m) {
 	
 	switch(request_struct->op_type) {
 		
-		case SET_DIRTY:
+		case SAC_CWP_Messages::SET_DIRTY:
 			response_struct = WP_set_dirty(request_struct);
 			break;
-		case CHECK_VALIDITY_DIRTY:
+		case SAC_CWP_Messages::CHECK_VALIDITY_DIRTY:
 			response_struct = WP_check_validity_dirty(request_struct);
 			break;
-		case CHECK_DATA_VALIDITY:
+		case SAC_CWP_Messages::CHECK_DATA_VALIDITY:
 			response_struct = WP_check_data_validity(request_struct);
 			break;
-		case INVALID_LINE:
+		case SAC_CWP_Messages::INVALID_LINE:
 			response_struct = WP_invalid_line(request_struct);
 			break;
-		case LOAD:
+		case SAC_CWP_Messages::LOAD:
 			response_struct = WP_load(request_struct);
 			break;
-		case STORE:
+		case SAC_CWP_Messages::STORE:
 			response_struct = WP_store(request_struct);
 			break;		
-		case WRITE_WITH_POLICIES:
+		case SAC_CWP_Messages::WRITE_WITH_POLICIES:
 			response_struct = WP_write_with_policies(request_struct);
 			break;			
 		default:
