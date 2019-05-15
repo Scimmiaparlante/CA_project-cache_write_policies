8745d94dbb70bfb31a12b62b8b636e039e436bd4
Raffa24 <47455930+Raffa24@users.noreply.github.com>
2019-05-15 17:22:28 +0200

    update test

--- test/CWP_test.cpp
+++ test/CWP_test.cpp
@@ -1,3 +1,4 @@
+#include<cstring>
 #include "CacheWritePolicies.h"
 
 /*
@@ -8,51 +9,59 @@ void testCache(CacheWritePolicies c, const char *s){
 	message m;
 	uint16_t* d = new uint16_t[16];
 	
-	m.id = 0;
-	m.source = "test";
-	m.dest = s;
+	for (int i=0; i<16; ++i)
+		d[i] = 0x0000;
 	
-	request.op_type = STORE;			// deve restituire LOAD_RECALL o CHECK_NEXT
+	m.id = 0;
+	strcpy(m.source, "test");
+	strcpy(m.dest, s);
+	
+	//reply with LOAD_RECALL in case of write_allocate (1, 2)
+	//CHECK_NEXT in case of write_no_allocate		   (3, 4)
+	request.op_type = STORE;			
 	request.address = 0x1111;
 	request.data = d;
-	m.magic_struct = (void*)request;
+	m.magic_struct = (void*)&request;
 	c.onNotify(&m);
 	
 	request.op_type = CHECK_DIRTY;
 	request.address = 0x1111;
-	m.magic_struct = (void*)request;
+	m.magic_struct = (void*)&request;
 	c.onNotify(&m);
 	
+	//reply with 0
 	request.op_type = INVALID_LINE;
 	request.address = 0x1111;
-	m.magic_struct = (void*)request;
-	c.onNotify(&m);						// mi deve dire 0
+	m.magic_struct = (void*)&request;
+	c.onNotify(&m);						
 	
 	request.op_type = LOAD;
 	request.address = 0x1111;
-	m.magic_struct = (void*)request;
+	m.magic_struct = (void*)&request;
 	c.onNotify(&m);
 	
 	request.op_type = INVALID_LINE;
 	request.address = 0x1111;
-	m.magic_struct = (void*)request;
+	m.magic_struct = (void*)&request;
 	c.onNotify(&m);
 	
+	//reply with PROPAGATE in case of write_through (2, 4)
+	//NO_PROPAGATE in case of write_back			(1, 3)
 	d[0] = 0xFFFF;
-	request.op_type = WRITE_WITH_POLICIES;	// deve restituire PROPAGATE o NO_PROPAGATE
+	request.op_type = WRITE_WITH_POLICIES;	
 	request.address = 0x1111;
 	request.data = d;
-	m.magic_struct = (void*)request;
+	m.magic_struct = (void*)&request;
 	c.onNotify(&m);
 	
 	request.op_type = CHECK_VALIDITY_DIRTY;
 	request.address = 0x1111;
-	m.magic_struct = (void*)request;
+	m.magic_struct = (void*)&request;
 	c.onNotify(&m);
 	
 	request.op_type = CHECK_DATA_VALIDITY;
 	request.address = 0x1111;
-	m.magic_struct = (void*)request;
+	m.magic_struct = (void*)&request;
 	c.onNotify(&m);
 }
 
@@ -62,7 +71,7 @@ int main(){
 						cache3("cache3", 1, 4096, 64, WRITE_BACK, WRITE_NO_ALLOCATE),
 						cache4("cache4", 1, 4096, 64, WRITE_THROUGH, WRITE_NO_ALLOCATE);
 	
-	// the tests are repeated for all the possible combinations of cache policies
+	//the tests are repeated for all the possible combinations of cache policies
 	testCache(cache1, "cache1");
 	testCache(cache2, "cache2");
 	testCache(cache3, "cache3");
