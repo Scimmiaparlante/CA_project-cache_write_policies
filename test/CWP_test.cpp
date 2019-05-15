32370d330ef285f79a9d21c6bd7f9632c20f8c98
Raffa24 <47455930+Raffa24@users.noreply.github.com>
2019-05-15 16:16:57 +0200

    Update test

--- test/CWP_test.cpp
+++ test/CWP_test.cpp
@@ -1,54 +1,68 @@
 #include "CacheWritePolicies.h"
 
-int main(){
-	CacheWritePolicies cache1("cache1", 1, 4096, 64, WRITE_BACK, WRITE_ALLOCATE);
-	
+void testCache(CacheWritePolicies c, const char *s){
 	SAC_to_CWP	request;
-	CWP_to_SAC* response;
 	message m;
 	uint16_t* d = new uint16_t[16];
 	
 	m.id = 0;
 	m.source = "test";
-	m.dest = "cache1";
+	m.dest = s;
 	
-	request.op_type = STORE;
+	request.op_type = STORE;			// deve restituire load o check next
 	request.address = 0x1111;
 	request.data = d;
 	m.magic_struct = (void*)request;
-	cache1.onNotify(&m);
+	c.onNotify(&m);
 	
 	request.op_type = CHECK_DIRTY;
 	request.address = 0x1111;
 	m.magic_struct = (void*)request;
-	cache1.onNotify(&m);
+	c.onNotify(&m);
 	
 	request.op_type = INVALID_LINE;
 	request.address = 0x1111;
 	m.magic_struct = (void*)request;
-	cache1.onNotify(&m);// mi deve dire 0
+	c.onNotify(&m);						// mi deve dire 0
 	
 	request.op_type = LOAD;
 	request.address = 0x1111;
 	m.magic_struct = (void*)request;
-	cache1.onNotify(&m);
+	c.onNotify(&m);
+	
+	request.op_type = INVALID_LINE;
+	request.address = 0x1111;
+	m.magic_struct = (void*)request;
+	c.onNotify(&m);
 	
 	d[0] = 0xFFFF;
-	request.op_type = WRITE_WITH_POLICIES;
+	request.op_type = WRITE_WITH_POLICIES;	// deve restituire propagate o no
 	request.address = 0x1111;
 	request.data = d;
 	m.magic_struct = (void*)request;
-	cache1.onNotify(&m);
+	c.onNotify(&m);
 	
 	request.op_type = CHECK_VALIDITY_DIRTY;
 	request.address = 0x1111;
 	m.magic_struct = (void*)request;
-	cache1.onNotify(&m);
+	c.onNotify(&m);
 	
 	request.op_type = CHECK_DATA_VALIDITY;
 	request.address = 0x1111;
 	m.magic_struct = (void*)request;
-	cache1.onNotify(&m);
+	c.onNotify(&m);
+}
+
+int main(){
+	CacheWritePolicies 	cache1("cache1", 1, 4096, 64, WRITE_BACK, WRITE_ALLOCATE),
+						cache2("cache2", 1, 4096, 64, WRITE_THROUGH, WRITE_ALLOCATE),
+						cache3("cache3", 1, 4096, 64, WRITE_BACK, WRITE_NO_ALLOCATE),
+						cache4("cache4", 1, 4096, 64, WRITE_THROUGH, WRITE_NO_ALLOCATE);
+	
+	testCache(cache1, "cache1");
+	testCache(cache2, "cache2");
+	testCache(cache3, "cache3");
+	testCache(cache4, "cache4");
 	
 	return 0;
 }
