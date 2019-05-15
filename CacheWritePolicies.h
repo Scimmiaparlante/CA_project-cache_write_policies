81e2d869732ca1619059a2d4fe88f3d8b7d537bd
Scimmiaparlante <Matteo@DESKTOP-H7M02I7>
2019-05-15 15:39:36 +0200

    mod1

--- CacheWritePolicies.h
+++ CacheWritePolicies.h
@@ -5,7 +5,7 @@
 
 #include "orchestrator/structures.h"
 #include "orchestrator/module.h"
-#inlcude "dm_cache.hh"
+#include "dm_cache.hh"
 
 #include "SetAssociative_WritePolicies_message.h"
 
@@ -13,7 +13,7 @@ enum HIT_POLICY {WRITE_BACK, WRITE_THROUGH};
 enum MISS_POLICY {WRITE_ALLOCATE, WRITE_NO_ALLOCATE};
 
 
-class CacheWritePolicies : public Cache, public module {
+class CacheWritePolicies : public class Cache, public class module {
 	
 	HIT_POLICY hit_policy;
 	MISS_POLICY miss_policy;
@@ -34,7 +34,7 @@ public:
 
 	CacheWritePolicies(string name, int priority, uint16_t cache_size, uint16_t line_size, HIT_POLICY hp, MISS_POLICY mp);
 	void onNotify(message *m);	
-}
+};
 
 
 
